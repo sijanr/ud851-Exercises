3ce9e752ff8e6ede398cf511d4180947ca6c1a06
sijanr <47872289+sijanr@users.noreply.github.com>
2020-05-07 22:02:53 -0700

    Complete To-Do items L02_E04

--- Lesson02-GitHub-Repo-Search/T02.04-Exercise-ConnectingToTheInternet/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.04-Exercise-ConnectingToTheInternet/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -16,15 +16,27 @@
 package com.example.android.datafrominternet;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.android.datafrominternet.utilities.NetworkUtils;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,14 +61,44 @@ public class MainActivity extends AppCompatActivity {
      * This method retrieves the search text from the EditText, constructs
      * the URL (using {@link NetworkUtils}) for the github repository you'd like to find, displays
      * that URL in a TextView, and finally fires off an AsyncTask to perform the GET request using
-     * our (not yet created) {@link GithubQueryTask}
+     * our (not yet created) {}
      */
     private void makeGithubSearchQuery() {
         String githubQuery = mSearchBoxEditText.getText().toString();
         URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
         mUrlDisplayTextView.setText(githubSearchUrl.toString());
-        // TODO (2) Call getResponseFromHttpUrl and display the results in mSearchResultsTextView
-        // TODO (3) Surround the call to getResponseFromHttpUrl with a try / catch block to catch an IOException
+        getResponse(githubSearchUrl);
+    }
+
+    private String getResponse(URL url) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(!response.isSuccessful()){
+                    throw new IOException("Unexpected code" + response);
+                } else {
+
+                    stringBuilder.append(response.body().string());
+                    Log.d("HeyHey", "onResponse: " + stringBuilder.toString());
+                    NetworkUtils.readJson(stringBuilder.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                call.cancel();
+                e.printStackTrace();
+            }
+        });
+
+        return stringBuilder.toString();
     }
 
     @Override
--- Lesson02-GitHub-Repo-Search/T02.04-Exercise-ConnectingToTheInternet/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ Lesson02-GitHub-Repo-Search/T02.04-Exercise-ConnectingToTheInternet/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -16,6 +16,14 @@
 package com.example.android.datafrominternet.utilities;
 
 import android.net.Uri;
+import android.util.Log;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,6 +31,12 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * These utilities will be used to communicate with the network.
@@ -63,29 +77,62 @@ public class NetworkUtils {
         return url;
     }
 
-    /**
-     * This method returns the entire result from the HTTP response.
-     *
-     * @param url The URL to fetch the HTTP response from.
-     * @return The contents of the HTTP response.
-     * @throws IOException Related to network and stream reading
-     */
-    public static String getResponseFromHttpUrl(URL url) throws IOException {
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try {
-            InputStream in = urlConnection.getInputStream();
+//    /**
+//     * This method returns the entire result from the HTTP response.
+//     *
+//     * @param url The URL to fetch the HTTP response from.
+//     * @return The contents of the HTTP response.
+//     * @throws IOException Related to network and stream reading
+//     */
+//    public static String getResponseFromHttpUrl(URL url) throws IOException {
+//        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//        try {
+//            InputStream in = urlConnection.getInputStream();
+//
+//            Scanner scanner = new Scanner(in);
+//            scanner.useDelimiter("\\A");
+//
+//            boolean hasInput = scanner.hasNext();
+//            if (hasInput) {
+//                return scanner.next();
+//            } else {
+//                return null;
+//            }
+//        } finally {
+//            urlConnection.disconnect();
+//        }
+//    }
 
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
 
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                return scanner.next();
-            } else {
-                return null;
-            }
-        } finally {
-            urlConnection.disconnect();
+
+//    public static void readJson(String jsonString) {
+//        if(jsonString.length()>0) {
+//            try {
+//                JSONObject root = new JSONObject(jsonString);
+//                int totalCount = root.getInt("total_count");
+//                Log.d("HeyHey", "readJson: Total count: " + totalCount);
+//            } catch (JSONException e) {
+//                Log.e("HeyHey","readJson: Error");
+//                e.printStackTrace();
+//            }
+//        } else {
+//            Log.e("HeyHey", "readJson: Empty string");
+//        }
+//    }
+
+    public static void readJson(String jsonString) throws IOException {
+        if(jsonString.length()>0) {
+            Moshi moshi = new Moshi.Builder().build();
+            JsonAdapter<RepositorySearchResult> jsonAdapter = moshi.adapter(RepositorySearchResult.class);
+
+            RepositorySearchResult repositorySearchResult = jsonAdapter.fromJson(jsonString);
+            Log.d("HeyHey", "readJson: Total count: " + repositorySearchResult.total_count);
+        } else {
+            Log.d("HeyHey", "readJson: Empty string");
         }
+    }
+
+    private static class RepositorySearchResult {
+        int total_count;
     }
 }
